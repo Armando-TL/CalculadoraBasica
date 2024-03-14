a6b98a015bd016477b0f18b40776b6e64f242732
amdtr <amdtr@PC-AMD>
2024-03-14 18:03:39 -0500

    Agragando teclado

---
+++ src/Calcular/Main.java
@@ -0,0 +1,11 @@
+package Calcular;
+// @author amdtr
+
+public class Main {
+    public static void main(String[] args) {
+        Calcular ventana = new Calcular();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+
+}
