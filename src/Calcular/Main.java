2127eda6f2fa02e4886c80697e982a376a34d42f
amdtr <amdtr@PC-AMD>
2024-03-19 16:51:16 -0500

    version con script manager

--- src/Calcular/Main.java
+++ src/Calcular/Main.java
@@ -12,8 +12,6 @@ public class Main {
         ventana.setVisible(true);
         ventana.setTitle("Calculadora AMD");
         ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        
-        
         // calcular tiene la vista y los metodos de mouse click y controlTeclado los de teclado
     }
 
